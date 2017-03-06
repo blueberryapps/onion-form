1d8e92a3a4c6a843c64505d9a1135ed72381d512
Ondrej Bartas <ondrej@bartas.cz>
2017-03-06 12:51:37 +0100

    Fix test suite

--- test/Button.spec.js
+++ test/Button.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-multi-comp */
 
 import React from 'react';
-import Submit from '../src/Submit.react';
+import Button from '../src/Button.react';
 import TestUtils from 'react-addons-test-utils';
 import { assert } from 'chai';
 import { jsdom } from 'jsdom';
@@ -9,22 +9,21 @@ import { jsdom } from 'jsdom';
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 
-describe('Submit', () => {
+describe('Button', () => {
   const container = TestUtils.renderIntoDocument(
-    <Submit disabled onClick={() => {}}>Send</Submit>
+    <Button disabled onClick={() => {}}>Send</Button>
   );
-
-  const submit = TestUtils.findRenderedComponentWithType(container, Submit);
+  const button = TestUtils.findRenderedComponentWithType(container, Button);
 
   it('should have children prop', () => {
-    assert.equal(submit.props.children, 'Send');
+    assert.equal(button.props.children, 'Send');
   });
 
   it('should have onClick in prop', () => {
-    assert.typeOf(submit.props.onClick, 'function');
+    assert.typeOf(button.props.onClick, 'function');
   });
 
   it('should have disabled in prop', () => {
-    assert.isTrue(submit.props.disabled);
+    assert.isTrue(button.props.disabled);
   });
 });
