9f3b8a9e393a41fcd16b46b02c31ccff03168af7
Antonin <antonin.hackenberg@gmail.com>
2017-05-23 17:47:49 +0200

    Reducer with types

--- src/types.js
+++ src/types.js
@@ -2,6 +2,10 @@
 
 export type ValueType = any;
 
+export type ValuesType = {
+  [string]: ValueType
+}
+
 export type PropertyType =
   | 'apiError'
   | 'error'
@@ -15,4 +19,30 @@ export type ActionType =
   | { type: 'CLEAR_ONION_FORM_PROPERTY', form: string, property: PropertyType }
   | { type: 'REGISTER_ONION_FORM_FIELD', form: string, field: string }
   | { type: 'SET_ONION_FORM_FIELD_PROPERTY', form: string, field: string, property: PropertyType, value: ValueType }
-  | { type: 'SET_ONION_FORM_MULTIPLE_FIELDS', form: string, property: PropertyType, values: Array<ValueType> };
+  | { type: 'SET_ONION_FORM_MULTIPLE_FIELDS', form: string, property: PropertyType, values: ValuesType};
+
+
+// Data
+
+export type FieldType = {
+  value: string,
+  liveValidation: boolean,
+  error: null | string,
+  apiError: null | null
+}
+
+export type FieldsType = {
+  [string]: FieldType
+}
+
+export type FieldsFormType = {
+  [string]: {
+    [string]: FieldType
+  }
+}
+
+export type StateType = {
+  fields: FieldsFormType
+}
+
+
