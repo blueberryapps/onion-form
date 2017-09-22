8ea50ac123c385b6aa01d897ea2fb6bc605ffe40
Jan Brecka <jan.brecka@gmail.com>
2017-09-22 17:52:58 +0200

    switch to enzyme

--- test/Field.spec.js
+++ test/Field.spec.js
@@ -4,10 +4,9 @@ import Field, { BasicInput } from '../src/Field.react';
 import Form from '../src/Form.react';
 import React from 'react';
 import reducer from '../src/reducer';
-import TestUtils from 'react-addons-test-utils';
 import { createStore } from 'redux';
-import { jsdom } from 'jsdom';
 import { Provider as ReduxProvider } from 'react-redux';
+import { mount } from 'enzyme';
 
 const initial = {
   fields: {
@@ -23,49 +22,45 @@ const initial = {
 
 const store = createStore(() => ({ onionForm: reducer(initial) }));
 
-global.document = jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
-
 describe('Field', () => {
   function createStub(customProps = {}, Input) {
-    const container = TestUtils.renderIntoDocument(
+    const wrapper = mount(
       <ReduxProvider store={store}>
         <Form name="fooForm">
           <Field name="firstName" {...customProps} />
         </Form>
       </ReduxProvider>
     );
-
-    return TestUtils.findRenderedComponentWithType(container, Input);
+    return wrapper.find(Input);
   }
 
   const stub = createStub({}, BasicInput);
 
   it('input should have name prop', () => {
-    expect(stub.props.name).toBe('firstName');
+    expect(stub.prop('name')).toBe('firstName');
   });
 
   it('input should have onChange prop', () => {
-    expect(typeof stub.props.onChange).toBe('function');
+    expect(typeof stub.prop('onChange')).toBe('function');
   });
 
   it('input should have value prop', () => {
-    expect(stub.props.value).toBe('Bar');
+    expect(stub.prop('value')).toBe('Bar');
   });
 
   it('input should have error prop', () => {
-    expect(stub.props.error).toBe('isRequired');
+    expect(stub.prop('error')).toBe('isRequired');
   });
 
   it('input should have customProperty prop', () => {
-    expect(stub.props.customProperty).toBe('Hi Hello From State');
+    expect(stub.prop('customProperty')).toBe('Hi Hello From State');
   });
 
   describe('with custom Component', () => {
     const FooInput = () => (<div className="FooInput" />);
 
     it('should render it', () => {
-      const fooContainer = TestUtils.renderIntoDocument(
+      const wrapperFoo = mount(
         <ReduxProvider store={store}>
           <Form name="fooForm">
             <Field name="firstName" component={FooInput} />
@@ -73,8 +68,7 @@ describe('Field', () => {
           </Form>
         </ReduxProvider>
       );
-
-      TestUtils.scryRenderedDOMComponentsWithClass(fooContainer, 'FooInput');
+      expect(wrapperFoo).toMatchSnapshot();
     });
   });
 });
--- test/integration.spec.js
+++ test/integration.spec.js
@@ -6,14 +6,10 @@ import Form from '../src/Form.react';
 import React from 'react';
 import reducer from '../src/reducer';
 import Button from '../src/Button.react';
-import TestUtils from 'react-addons-test-utils';
 import { createStore } from 'redux';
-import { jsdom } from 'jsdom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { TextField, CheckBox } from './mocks';
-
-global.document = jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
+import { mount } from 'enzyme';
 
 const isRequired = (value) => ((value && `${value}`.length > 0) ? null : 'required');
 
@@ -26,7 +22,7 @@ describe('Integration', () => {
   function createSubmit(fooForm = {}, validations = {}) {
     const store = createStore((state = { onionForm: { fields: { fooForm } } }, action) => ({ onionForm: reducer(state.onionForm, action) }));
 
-    const container = TestUtils.renderIntoDocument(
+    const wrapper = mount(
       <ReduxProvider store={store}>
         <Form name="fooForm">
           <Name validations={validations.name} />
@@ -36,36 +32,36 @@ describe('Integration', () => {
       </ReduxProvider>
     );
 
-    return TestUtils.findRenderedComponentWithType(container, Button);
+    return wrapper.find(Button);
   }
 
   it('should have valid true when no validations', () => {
     const submit = createSubmit();
-    expect(submit.props.valid).toBe(true);
+    expect(submit.prop('valid')).toBe(true);
   });
 
   it('should have valid true when all fields are valid (one field)', () => {
     const submit = createSubmit({ name: { value: 'John' } }, { name: [isRequired] });
-    expect(submit.props.valid).toBe(true);
+    expect(submit.prop('valid')).toBe(true);
   });
 
   it('should have valid true when all fields are valid (two fields)', () => {
     const submit = createSubmit({ name: { value: 'John' }, accept: { value: true } }, { name: [isRequired], accept: [isRequired] });
-    expect(submit.props.valid).toBe(true);
+    expect(submit.prop('valid')).toBe(true);
   });
 
   it('should have valid false when all fields are not valid (invalid value)', () => {
     const submit = createSubmit({ name: { value: 'John' }, accept: { value: false } }, { name: [isRequired], accept: [isRequired] });
-    expect(submit.props.valid).toBe(false);
+    expect(submit.prop('valid')).toBe(false);
   });
 
   it('should have valid false when all fields are not valid (missing value in state)', () => {
     const submit = createSubmit({ name: { value: '' } }, { name: [isRequired], accept: [isRequired] });
-    expect(submit.props.valid).toBe(false);
+    expect(submit.prop('valid')).toBe(false);
   });
 
   it('should have valid false when all fields are not valid (missing all values)', () => {
     const submit = createSubmit({}, { name: [isRequired], accept: [isRequired] });
-    expect(submit.props.valid).toBe(false);
+    expect(submit.prop('valid')).toBe(false);
   });
 });
