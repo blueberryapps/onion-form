32948f08d041aa0751040f263f59a24215d3d6f3
Jan Brecka <jan.brecka@gmail.com>
2017-09-22 17:35:59 +0200

    switch to enzyme

--- test/Button.spec.js
+++ test/Button.spec.js
@@ -2,27 +2,20 @@
 
 import React from 'react';
 import Button from '../src/Button.react';
-import TestUtils from 'react-addons-test-utils';
-import { jsdom } from 'jsdom';
-
-global.document = jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
+import { shallow } from 'enzyme';
 
 describe('Button', () => {
-  const container = TestUtils.renderIntoDocument(
-    <Button disabled onClick={() => {}}>Send</Button>
-  );
-  const button = TestUtils.findRenderedComponentWithType(container, Button);
+  const wrapper = shallow(<Button disabled onClick={() => {}}>Send</Button>);
 
   it('should have children prop', () => {
-    expect(button.props.children).toBe('Send');
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should have onClick in prop', () => {
-    expect(typeof button.props.onClick).toBe('function');
+    expect(typeof wrapper.prop('onClick')).toBe('function');
   });
 
   it('should have disabled in prop', () => {
-    expect(button.props.disabled).toBe(true);
+    expect(wrapper.prop('disabled')).toBe(true);
   });
 });
